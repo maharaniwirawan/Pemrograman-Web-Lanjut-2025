f6eb85fde5feb05c658946f021c10f284a56c87c
Maharani Wirawan <maharaniwr246@gmail.com>
2025-03-03 23:35:48 +0700

    PWL_POS

--- Week 3/PWL_POS/routes/web.php
+++ Week 3/PWL_POS/routes/web.php
@@ -1,9 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\HomeController;
-use App\Http\Controllers\ProductController;
-use App\Http\Controllers\SalesController;
+use App\Http\Controllers\LevelController;
+use App\Http\Controllers\KategoriController;
 use App\Http\Controllers\UserController;
 
 /*
@@ -21,16 +20,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/home', [HomeController::class, 'index'])->name('home');
-
-Route::prefix('category')->group(function () {
-    Route::get('/food-beverage', [ProductController::class, 'foodBeverage'])->name('category.food-beverage');
-    Route::get('/beauty-health', [ProductController::class, 'beautyHealth'])->name('category.beauty-health');
-    Route::get('/home-care', [ProductController::class, 'homeCare'])->name('category.home-care');
-    Route::get('/baby-kid', [ProductController::class, 'babyKid'])->name('category.baby-kid');
-});
-
-Route::get('/user/{id}/name/{name}', [UserController::class, 'show'])->name('user.show');
-
-Route::get('/sales', [SalesController::class, 'index'])->name('sales.index');
-
+Route::get('/level', [LevelController::class, 'index']);
+Route::get('/kategori', [KategoriController::class, 'index']);
+Route::get('/user', [UserController::class, 'index']);
