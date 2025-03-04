ad17225a746ebbc49d13f92a47857d62b21f160e
Maharani Wirawan <maharaniwr246@gmail.com>
2025-03-04 09:23:55 +0700

    menambahkan penjelasan kode

--- Week 3/PWL_POS/database/migrations/2025_03_03_134323_create_m_penjualan_detail_table.php
+++ Week 3/PWL_POS/database/migrations/2025_03_03_134323_create_m_penjualan_detail_table.php
@@ -14,7 +14,12 @@ return new class extends Migration
         Schema::create('t_penjualan_detail', function (Blueprint $table) {
             $table->id('detail_id');
             $table->foreignId('penjualan_id')->constrained('t_penjualan', 'penjualan_id')->onDelete('cascade');
+            //membuat kolom penjualan_id sebagai foreignkey
+            //(constrained) foreign key merujuk ke kolom penjualan_id pada tabel t_penjualan
+            //(onDelete('cascade')) jika data tabel t_penjuaan dihapus maka data penjualan_id yang sama pada tabel juga ikut terhapus otomatis
             $table->foreignId('barang_id')->constrained('m_barang', 'barang_id');
+            //membuat kolom barang_id sebagai foreignkey
+            //(constrained) foreign key merujuk ke kolom barang_id pada tabel m_barang
             $table->integer('harga');
             $table->integer('jumlah');
             $table->timestamps();
