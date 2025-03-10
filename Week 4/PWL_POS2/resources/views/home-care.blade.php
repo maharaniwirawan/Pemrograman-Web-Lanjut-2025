168e0d6e2919993365bee41b01e703471165a132
Maharani Wirawan <maharaniwr246@gmail.com>
2025-03-10 18:48:56 +0700

    Jobsheet 4

---
+++ Week 4/PWL_POS2/resources/views/home-care.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Home & Care</title>
+</head>
+<body>
+    <h1>Daftar Produk: Home & Care</h1>
+    <ul>
+        <li>🧼 Sabun - Rp 10.000</li>
+        <li>🌸 Pengharum Ruangan - Rp 35.000</li>
+        <li>👚 Parfum Baju - Rp 20.000</li>
+    </ul>
+</body>
+</html>
