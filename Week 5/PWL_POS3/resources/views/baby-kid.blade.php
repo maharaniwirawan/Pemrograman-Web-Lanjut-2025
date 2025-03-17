380b248ccf0bca95c93e0f0c58eafd1a4b5f9cae
Maharani Wirawan <maharaniwr246@gmail.com>
2025-03-17 22:50:49 +0700

    Jobsheet5

---
+++ Week 5/PWL_POS3/resources/views/baby-kid.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Baby & Kid</title>
+</head>
+<body>
+    <h1>Daftar Produk: Baby & Kid</h1>
+    <ul>
+        <li>🎒 Tas Sekolah Anak - Rp 150.000</li>
+        <li>🚗 Mainan anak - Rp 50.000</li>
+        <li>🍼 Susu Formula - Rp 45.000</li>
+    </ul>
+</body>
+</html>
