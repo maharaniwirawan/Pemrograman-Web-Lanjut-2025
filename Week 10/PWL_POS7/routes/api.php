c9d7da33b17dff297c6573c7098d4de664ab9a9d
Maharani Wirawan <maharaniwr246@gmail.com>
2025-04-24 10:54:16 +0700

    Jobsheet 10 Praktikum 1

---
+++ Week 10/PWL_POS7/routes/api.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\SupplierController;
+use App\Http\Controllers\BarangController;
+use App\Http\Controllers\KategoriController;
+use App\Http\Controllers\Api\RegisterController;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
+
+Route::post('/register', [RegisterController::class, 'register']);
+
+Route::post('/register', App\Http\Controllers\Api\RegisterController::class)->name('register');
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+// Route::get('/users/data', [UserController::class, 'getData']);
+
+// Route::get('/kategori/data', [KategoriController::class, 'data'])->name('kategori.data');
+// Route::get('/barang/data', [BarangController::class, 'data'])->name('barang.data');
+// Route::get('/supplier/data', [SupplierController::class, 'data'])->name('supplier.data');
+
+
